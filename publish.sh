019f06b2036d523551a6713fe14e61f343c991e5
DaphnisLi <1872758972@qq.com>
2022-08-18 17:51:19 +0800

    feat: 增加 release 脚本

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '开始构建脚手架 🙄️'
-
-npm run build
-
-echo '脚手架构建完成, 现在发布 😁'
-
-npm publish --access public
---
+++ scripts/release.js
@@ -0,0 +1,51 @@
+import { prompt } from 'inquirer'
+import { inc } from 'semver'
+import { echo, exec } from 'shelljs'
+import { green, yellow } from 'chalk'
+const { version } = require('../package.json')
+
+// 自动算出要升的版本号
+const major = inc(version, 'major') // 主版本 (重大版本改动)
+const minor = inc(version, 'minor') // 次版本 (feat)
+const patch = inc(version, 'patch') // 补丁版本 (bug)
+
+// 交互命令行
+prompt([
+  {
+    type: 'list', // 单选
+    message: '选择你要发布的版本号',
+    name: 'releaseVersion',
+    choices: [
+      {
+        name: `主版本: v${version} => v${major}`,
+        value: 'major' // 既可以是字符串也可以是数字
+      },
+      {
+        name: `次版本: v${version} => v${minor}`,
+        value: 'minor'
+      },
+      {
+        name: `补丁版本: v${version} => v${patch}`,
+        value: 'patch'
+      }
+    ]
+  }
+]).then(({ releaseVersion }) => {
+  echo(yellow('修改版本号 😁'))
+  // 修改版本号, 禁用版本提交和标记tag。其实直接 write package 也是可以的, 但是太粗暴了。
+  exec(`npm version ${releaseVersion} --no-git-tag-version`) // releaseVersion = 'major' or 1.0.0
+
+  echo(yellow('Changelog 😁'))
+  exec('npm run changelog')
+
+  echo(yellow('提交代码 😁'))
+  exec(`git add . && git commit -m 'release: V ${releaseVersion}' && git push origin HEAD`)
+
+  echo(yellow('设置 Tag 😁'))
+  exec(`git tag v${releaseVersion} && git push origin V ${releaseVersion}`)
+
+  echo(yellow('发布 npm 😁'))
+  exec('npm run build && npm publish --access public')
+
+  echo(green('大功告成 🧨🧨🧨🧨🧨'))
+})
